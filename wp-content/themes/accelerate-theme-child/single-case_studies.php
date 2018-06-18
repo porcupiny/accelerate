8b3b76c7b905762c0cc159012ffd69045f083733
porcupiny <liane.chan@gmail.com>
2018-06-18 11:04:53 -0700

    added Advanced Custom Fields to Case Studies

--- wp-content/themes/accelerate-theme-child/single-case_studies.php
+++ wp-content/themes/accelerate-theme-child/single-case_studies.php
@@ -16,9 +16,39 @@ get_header(); ?>
 
 	<div id="primary" class="site-content">
 		<div class="main-content" role="main">
-			<?php while ( have_posts() ) : the_post(); ?>
+			<?php while ( have_posts() ) : the_post();
+
+				$services = get_field('services');
+				$client = get_field('client');
+				$link = get_field('site_link');
+				$image_1 = get_field('image_1');
+				$image_2 = get_field('image_2');
+				$image_3 = get_field('image_3');  
+				$size = "full";?>
+
 				<article class="case-study">
-				<?php the_content(); ?></article>
+					<aside class="case-study-sidebar">
+						<h2><?php the_title(); ?></h2>
+						<h5><?php echo $services; ?></h5>
+						<h6>Client: <?php echo $client; ?></h6>
+					
+						<?php the_content(); ?>
+
+						<p><a class="read-more-link" href="<?php echo $link; ?>">Site Link</a></p>
+					</aside>
+					<div class="case-study-images">
+						<?php if($image_1) { ?>
+							<img src="<?php echo $image_1; ?>" />
+						<?php } ?>
+						<?php if($image_2) { ?>
+						<img src="<?php echo $image_2; ?>" /> 
+					<?php } ?>					
+
+						<?php if($image_3)  { ?>
+						<img src="<?php echo $image_3; ?>" /> 
+						<?php } ?>
+					</div>
+				</article>
 			<?php endwhile; // end of the loop. ?>
 		</div><!-- .main-content -->
 
